8c128038ceee461f0295fb1b41cebbca44bd8b49
Randy Lutcavich <rlutcavich@gmail.com>
2018-09-01 21:07:14 -0700

    - Changed main layout to be a RecyclerView - Setup RecyclerView in using the new adapter and a LinearLayoutManager

--- app/src/main/java/io/winf/simpletvguide/MainActivity.kt
+++ app/src/main/java/io/winf/simpletvguide/MainActivity.kt
@@ -2,11 +2,32 @@ package io.winf.simpletvguide
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = SingleChannelGuideAdapter(getMockProgramList())
+
+        recyclerView = findViewById<RecyclerView>(R.id.single_channel_guide_recycler_view).apply {
+            setHasFixedSize(true)
+
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
+    private fun getMockProgramList(): List<String> {
+        val programsArray = resources.getStringArray(R.array.mock_program_name_array)
+        return programsArray.toList()
     }
 }
