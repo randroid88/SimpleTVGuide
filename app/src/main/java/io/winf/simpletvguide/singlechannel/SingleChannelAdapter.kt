e8c04bc6ce808fee7af3eda3347b199f16f13dbb
Randy Lutcavich <rlutcavich@gmail.com>
2018-09-03 16:24:17 -0700

    - Moved class to a new feature package (singlechannel)

--- app/src/main/java/io/winf/simpletvguide/singlechannel/SingleChannelAdapter.kt
+++ app/src/main/java/io/winf/simpletvguide/singlechannel/SingleChannelAdapter.kt
@@ -1,9 +1,10 @@
-package io.winf.simpletvguide
+package io.winf.simpletvguide.singlechannel
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import io.winf.simpletvguide.R
 
 class SingleChannelAdapter(private val programList: List<String>) :
         RecyclerView.Adapter<SingleChannelAdapter.ProgramViewHolder>() {
@@ -11,7 +12,7 @@ class SingleChannelAdapter(private val programList: List<String>) :
     class ProgramViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): SingleChannelAdapter.ProgramViewHolder {
+                                    viewType: Int): ProgramViewHolder {
         val textView = LayoutInflater.from(parent.context)
                 .inflate(R.layout.program_text_view, parent, false) as TextView
 
