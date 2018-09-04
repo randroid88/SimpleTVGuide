94f2edda97f932ccccc4d7b85fa2307bac5fcb9e
Randy Lutcavich <rlutcavich@gmail.com>
2018-09-03 20:45:39 -0700

    - Added button to new MainFragment

--- app/src/main/java/io/winf/simpletvguide/MainFragment.kt
+++ app/src/main/java/io/winf/simpletvguide/MainFragment.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 
+class MainFragment : Fragment(), View.OnClickListener {
 
-class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val singleChannelButton = view.findViewById(R.id.single_channel_button) as Button
+        singleChannelButton.setOnClickListener(this)
+
+        return view
     }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented")
+    }
+
 }
